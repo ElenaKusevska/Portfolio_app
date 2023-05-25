5d6f14b5cd0df2a8a540f409a1aef51c0c8a5320
ElenaKusevska <elena.kusevska@gmail.com>
2023-05-25 15:27:44 +0200

    add flask route and html link for contact me page

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def index():
     return render_template("main_page.html")
 
+@app.route("/contactme")
+def contactme():
+    return render_template("contactme.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
